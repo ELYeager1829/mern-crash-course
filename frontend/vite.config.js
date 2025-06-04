9a2fde243e64adcfb26aac6901ec2ad617d48250
Lungile <lungilethobejane8@gmail.com>
2025-06-04 17:18:21 +0200

    need

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,12 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-   root: 'public',
+	root: './', // Ensure this points to your project root
+	build: {
+		rollupOptions: {
+			input: '/index.html' // Explicitly set if needed
+		}
+	},
 	server: {
 		proxy: {
 			"/api": {
